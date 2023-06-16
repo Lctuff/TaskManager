72a0941a0ae56659053d8f7bb3186ffda77d78da
Logan Chew <logantuff@gmail.com>
2023-06-16 11:59:03 -0600

    async code

--- routes/tasks.js
+++ routes/tasks.js
@@ -14,20 +14,23 @@ function validateTask(task){
 }
 
 router.post('/api/tasks', (req, res)=>{
-    const { error } = validateTask(req.body)
-    if(error){
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-    const task ={
-        id: tasks.length +1,
-        task: req.body.task
-    };
-    tasks.push(task);
-    res.send(task);
+    setTimeout(()=>{
+        const { error } = validateTask(req.body);
+        if(error){
+            res.status(400).send(error.details[0].message);
+            return;
+        }
+        const task ={
+            id: tasks.length +1,
+            task: req.body.task
+        };
+        tasks.push(task);
+        res.send(task);
+    }, 500);
 });
 
 router.delete('/api/tasks/:id', (req, res)=>{
+setTimeout(()=>{
     const task = tasks.find(t => t.id === parseInt(req.params.id));
     if(!task)return res.status(404).send('The task with the given ID was not found.');
 
@@ -35,23 +38,29 @@ router.delete('/api/tasks/:id', (req, res)=>{
     tasks.splice(index, 1);
 
     res.send(task);
+}, 500);
 });
 
 router.get('/api/tasks',(req,res)=>{
+setTimeout(()=>{
     res.send(tasks);
+}, 500);
 });
 router.get('/api/tasks/:id', (req,res)=>{
+setTimeout(() =>{
     const task = tasks.find(t => t.id === parseInt(req.params.id));
     if(!task)return res.status(404).send('The task with the given ID was not found.');
 
     res.send(task);
+}, 500);
 });
 
 router.put('/api/tasks/:id',(req, res)=>{
+setTimeout(()=>{
     const task = tasks.find(t => t.id === parseInt(req.params.id));
     if(!task)return res.status(404).send('The task with the given ID was not found.');
 
-    const { error } = validateTask(req.body)
+    const { error } = validateTask(req.body);
     if(error){
         res.status(400).send(error.details[0].message);
         return;
@@ -59,6 +68,6 @@ router.put('/api/tasks/:id',(req, res)=>{
 
     task.task = req.body.task;
     res.send(task); 
-
+}, 500);
 });
 module.exports = router;
